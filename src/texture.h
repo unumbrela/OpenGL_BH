1df93aa20d74037f01a63dd3888569fa029c6339
Zihao Guo <1191230428@stu.jiangnan.edu.cn>
2024-12-29 19:51:40 +0800

    Update texture.h

--- src/texture.h
+++ src/texture.h
@@ -1,14 +1,3 @@
-/**
- * @file render.h
- * @author Ross Ning (rossning92@gmail.com)
- * @brief Utility functions for GL texture management.
- * @version 0.1
- * @date 2020-08-29
- *
- * @copyright Copyright (c) 2020
- *
- */
-
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
