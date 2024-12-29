fe181f5c592cd9fccd2db20fa5edfb74680e22fe
Zihao Guo <1191230428@stu.jiangnan.edu.cn>
2024-12-29 19:51:28 +0800

    Update shader.h

--- src/shader.h
+++ src/shader.h
@@ -1,14 +1,3 @@
-/**
- * @file render.h
- * @author Ross Ning (rossning92@gmail.com)
- * @brief Utility functions for GL shader.
- * @version 0.1
- * @date 2020-08-29
- *
- * @copyright Copyright (c) 2020
- *
- */
-
 #ifndef SHADER_H
 #define SHADER_H
 
